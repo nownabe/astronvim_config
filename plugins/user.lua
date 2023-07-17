ed006b457812bebb6d5eb0439cff62c70e9cafc1
nownabe <nownabe@gmail.com>
2023-07-17 15:49:15 +0900

    Add some plugins

---
+++ plugins/user.lua
@@ -0,0 +1,19 @@
+return {
+  "vim-denops/denops.vim",
+  "folke/trouble.nvim",
+  {
+    "kat0h/bufpreview.vim",
+    dependencies = {"vim-denops/denops.vim"},
+    ft = {"markdown"},
+    build = "deno task prepare",
+  },
+  {
+    "dhruvasagar/vim-table-mode",
+    ft = {"markdown"},
+    config = function()
+      vim.g.table_mode_corner = "|"
+      vim.g.table_mode_header = "-"
+      vim.api.nvim_command("TableModeEnable")
+    end,
+  }
+}
